fa7b476a080efb13c4f4e1cf8660446567545135
Chely Ho <adept_inept@yahoo.com>
2018-02-12 00:06:55 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -10,7 +10,7 @@ class CashRegister
  def add_item(title, price, quantity = 1)
    @total += price * quantity
    count = 0
-   until count == quantity do 
+   until count == quantity do
      @items << title
      count += 1
    end
